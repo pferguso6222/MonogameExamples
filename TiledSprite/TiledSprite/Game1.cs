002a486485b3c68f0096abefd707e4541e0606ff
Pat Ferguson <pat.ferguson@acres4.com>
2018-10-05 21:11:03 -0700

    improving BitmapFonts.

--- TiledSprite/TiledSprite/Game1.cs
+++ TiledSprite/TiledSprite/Game1.cs
@@ -86,7 +86,7 @@ namespace TiledSprite
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.White);
+            GraphicsDevice.Clear(Color.BlanchedAlmond);
 
             myDialogBkg.Draw(spriteBatch);
 
