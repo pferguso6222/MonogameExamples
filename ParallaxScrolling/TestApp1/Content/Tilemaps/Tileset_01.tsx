b4d04be4666d1298e2cd3cc1910b65583c3d2a95
pferguso6222 <obstacledelusions@hotmail.com>
2019-04-30 10:37:03 -0700

    messing with Animated tiles.

---
+++ ParallaxScrolling/TestApp1/Content/Tilemaps/Tileset_01.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Tileset_01" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="Tileset_01.png" width="64" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="0" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
